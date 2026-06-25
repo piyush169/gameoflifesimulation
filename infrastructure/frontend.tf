3def53f29568066140acb4fc2f2af95dcac8642f
Piyush Singh <piyu1219@gmail.com>
2026-06-25 21:26:04 +0530

    Provision S3 bucket for frontend, upload files via CI, and enlarge UI

---
+++ infrastructure/frontend.tf
@@ -0,0 +1,43 @@
+resource "aws_s3_bucket" "frontend" {
+  bucket = "gol.piyu.me"
+}
+
+resource "aws_s3_bucket_website_configuration" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+  depends_on = [aws_s3_bucket_public_access_block.frontend]
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.frontend.arn}/*"
+      },
+    ]
+  })
+}
+
+output "frontend_website_endpoint" {
+  description = "The S3 Website Endpoint to point your Route53 CNAME to"
+  value       = aws_s3_bucket_website_configuration.frontend.website_endpoint
+}
